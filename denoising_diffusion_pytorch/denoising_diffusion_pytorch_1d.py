316cbcd8d2f9fd6edfb81c123a8945fe901f2d69
kirilzilla <kirilm@me.com>
2023-07-03 09:53:27 +0200

    adjusted denoising pytorch 1d.py so that it runs locally

--- denoising_diffusion_pytorch/denoising_diffusion_pytorch_1d.py
+++ denoising_diffusion_pytorch/denoising_diffusion_pytorch_1d.py
@@ -251,6 +251,86 @@ class Attention(nn.Module):
         return self.to_out(out)
 
 # model
+class Unet1D_custom(nn.Module):
+    def __init__(
+            self,
+            dim,
+            init_dim=None,
+            out_dim=None,
+            dim_mults=(1, 2, 4, 8),
+            channels=3, 
+            resnet_block_groups=8,
+    ):
+        super().__init__()
+
+        self.channels = channels
+        input_channels = channels
+
+        time_dim = None
+        init_dim = default(init_dim, dim)
+        self.init_conv = nn.Conv1d(input_channels, init_dim, 7, padding = 3)
+
+        dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        block_klass = partial(ResnetBlock, groups = resnet_block_groups)
+
+        # layers
+
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)
+
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(nn.ModuleList([
+                block_klass(dim_in, dim_in, time_emb_dim = time_dim),
+                block_klass(dim_in, dim_in, time_emb_dim = time_dim),
+                Residual(PreNorm(dim_in, LinearAttention(dim_in))),
+                Downsample(dim_in, dim_out) if not is_last else nn.Conv1d(dim_in, dim_out, 3, padding = 1)
+            ]))
+
+    def forward(self, x, time, x_self_cond = None):
+        if self.self_condition:
+            x_self_cond = default(x_self_cond, lambda: torch.zeros_like(x))
+            x = torch.cat((x_self_cond, x), dim = 1)
+
+        x = self.init_conv(x)
+        r = x.clone()
+
+        t = self.time_mlp(time)
+
+        h = []
+
+        for block1, block2, attn, downsample in self.downs:
+            x = block1(x, t)
+            h.append(x)
+
+            x = block2(x, t)
+            x = attn(x)
+            h.append(x)
+
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_attn(x)
+        x = self.mid_block2(x, t)
+
+        for block1, block2, attn, upsample in self.ups:
+            x = torch.cat((x, h.pop()), dim = 1)
+            x = block1(x, t)
+
+            x = torch.cat((x, h.pop()), dim = 1)
+            x = block2(x, t)
+            x = attn(x)
+
+            x = upsample(x)
+
+        x = torch.cat((x, r), dim = 1)
+
+        x = self.final_res_block(x, t)
+        return self.final_conv(x)
 
 class Unet1D(nn.Module):
     def __init__(
@@ -340,7 +420,7 @@ class Unet1D(nn.Module):
         self.final_res_block = block_klass(dim * 2, dim, time_emb_dim = time_dim)
         self.final_conv = nn.Conv1d(dim, self.out_dim, 1)
 
-    def forward(self, x, time, x_self_cond = None):
+    def forward(self, x, time=None, x_self_cond = None):
         if self.self_condition:
             x_self_cond = default(x_self_cond, lambda: torch.zeros_like(x))
             x = torch.cat((x_self_cond, x), dim = 1)
@@ -348,7 +428,7 @@ class Unet1D(nn.Module):
         x = self.init_conv(x)
         r = x.clone()
 
-        t = self.time_mlp(time)
+        t =None#self.time_mlp(time)
 
         h = []
 
@@ -728,7 +808,7 @@ class Trainer1D(object):
         num_samples = 25,
         results_folder = './results',
         amp = False,
-        mixed_precision_type = 'fp16',
+        mixed_precision_type = False, #'fp16',
         split_batches = True,
     ):
         super().__init__()
@@ -737,8 +817,8 @@ class Trainer1D(object):
 
         self.accelerator = Accelerator(
             split_batches = split_batches,
-            mixed_precision = mixed_precision_type if amp else 'no'
-        )
+            mixed_precision = 'no'# mixed_precision_type if amp else 'no'
+        ) 
 
         # model
 
